754c984f5968b09b43c7c78ebe0d039df54f04d6
m5r <me@mokhtarmial.com>
2018-10-15 15:45:55 +0200

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const micro = require('micro');
+const Router = require('micro-http-router');
+
+const { send } = micro;
+const router = new Router();
+
+const results = {
+	one: 0,
+	five: 0,
+};
+
+router.route({
+	path: '/results',
+	method: 'GET',
+	handler: (req, res) => {
+		return send(res, 200, results);
+	},
+});
+
+router.route({
+	path: '/kill/:amount',
+	method: 'POST',
+	handler: (req, res) => {
+		const amount = req.params.amount;
+		results[amount]++;
+		return 'Successfully killed';
+	},
+});
+
+const server = micro((req, res) => router.handle(req, res));
+const port = 3029;
+server.listen(port);
+console.log(`micro is listening on port: ${port}`);
